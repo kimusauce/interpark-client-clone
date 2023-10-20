159e4be8841a8690810221364aa9743f36a4a52a
kimusauce <kimusauce@gmail.com>
2023-10-20 17:21:44 +0900

    feat : 프로젝트 파일 정리 및 최적화

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import Header from "./Header";
+
+export default function Footer() {
+  //JS area
+  return (
+    <footer class="header">하단</footer>
+    //HTML area
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  //JS area
+  return (
+    //HTML area
+    <header class="header">상단</header>
+  );
+}
