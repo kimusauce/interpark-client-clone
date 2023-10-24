53e7efa79c2db5938fef8e0532b0d2319b2a3ff2
kimusauce <kimusauce@gmail.com>
2023-10-24 16:01:10 +0900

    feat : header 영역 기초 완료

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import Header from "./Header";
-
-export default function Footer() {
-  //JS area
-  return (
-    <footer class="header">하단</footer>
-    //HTML area
-  );
-}
--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  //JS area
-  return (
-    //HTML area
-    <header class="header">상단</header>
-  );
-}
